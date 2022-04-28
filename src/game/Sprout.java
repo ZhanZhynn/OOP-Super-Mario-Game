3e559c6f9280f49184bfb9a7b6ce2f7b93f5576a
Zz <zhanzhynn@gmail.com>
2022-04-28 18:44:34 +0800

    Added Sprout class

--- src/game/Sprout.java
+++ src/game/Sprout.java
@@ -1,4 +1,27 @@
 package game;
 
-public class Sprout {
+import edu.monash.fit2099.engine.positions.Location;
+
+public class Sprout extends Tree {
+    private int counter;
+
+    public Sprout(){
+        super('+');
+        this.addCapability(Capabilities.SPAWN_GOOMBA);
+        counter = 0;
+    }
+
+    public void tick(Location location){
+        counter+=1;
+        if (counter % 10 == 0){
+            location.setGround(new Sapling());
+        }
+        if (!location.containsAnActor()) {
+            if (this.hasCapability(Capabilities.SPAWN_GOOMBA)) {
+                if (Math.random() <= 0.1)
+                    location.addActor(new Goomba());
+            }
+        }
+    }
+
 }
