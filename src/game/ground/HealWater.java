39e91de6c12e97b3818cca72f22aac8ceea34c3d
macbookpro <dhwa0003@student.monash.edu>
2022-05-13 17:47:45 +0800

    RefillAction and HealthFunction implemented.

---
+++ src/game/ground/HealWater.java
@@ -0,0 +1,15 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import game.interfaces.Water;
+
+public class HealWater implements Water {
+    @Override
+    public void drink(Actor actor) {
+        actor.heal(50);
+    }
+
+    public String toString(){
+        return "Healing water";
+    }
+}
--- src/game/ground/HealthFountain.java
+++ src/game/ground/HealthFountain.java
@@ -1,15 +1,48 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.action.JumpAction;
+import game.action.RefillAction;
+import game.actor.Player;
+import game.interfaces.Water;
+import game.item.Bottle;
 
-public class HealthFountain extends Ground {
 
+public class HealthFountain extends Ground{
+    private Bottle bottle;
     /**
      * Constructor.
      *
      * @param displayChar character to display for this type of terrain
      */
-    public HealthFountain(char displayChar) {
+    public HealthFountain(char displayChar, Bottle bottle) {
         super('H');
+        this.bottle = bottle;
     }
+
+    public Water getWater(){
+        return new HealWater();
+    }
+
+    /**
+     * At the moment, we only make it can be attacked by Player.
+     * You can do something else with this method.
+     * @param otherActor the Actor that might perform an action.
+     * @param location location of tree
+     * @param direction  String representing the direction of the other Actor
+     * @return list of actions
+     */
+    public ActionList allowableActions(Actor otherActor, Location location, String direction) {
+        ActionList actions = new ActionList();
+        if(otherActor == location.getActor()) {
+                actions.add(new RefillAction(getWater(), bottle));
+            }
+        return actions;
+    }
+
+
+
 }
