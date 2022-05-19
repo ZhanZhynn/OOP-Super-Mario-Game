501448f86e55a52c4d34125d1e7820549940e2fd
Zz <zhanzhynn@gmail.com>
2022-05-19 22:19:06 +0800

    Added more wrappipes

--- src/game/Application.java
+++ src/game/Application.java
@@ -76,25 +76,31 @@ public class Application {
 
 			Player mario = new Player("Player", 'm', 1000);
 			world.addPlayer(mario, gameMap.at(42, 10));
-//			mario.setLastLocation(gameMap.at(42, 10));
 
 			WarpPipe pipe = new WarpPipe(lavaMap.at(0,0),"to Lava Zone!!!");
-//			pipe.allowableActions(mario, lavaMap.at(0,0), " to Lava Zone!!");
-//			pipe.addTeleportActions(mario, lavaMap.at(0,0), " to Lava Zone!!!");
 			gameMap.at(40,13).setGround(pipe);
 
+			WarpPipe pipe2 = new WarpPipe(lavaMap.at(0,0),"to Lava Zone!!!");
+			gameMap.at(20,6).setGround(pipe2);
+
 			WarpPipe pipe3 = new WarpPipe(lavaMap.at(0,0),"to Lava Zone!!!");
-//			pipe3.addTeleportActions(mario, lavaMap.at(0,0), " to Lava Zone!!!");
-			gameMap.at(38,10).setGround(pipe3);
+			gameMap.at(38,5).setGround(pipe3);
+
+			WarpPipe pipe4 = new WarpPipe(lavaMap.at(0,0),"to Lava Zone!!!");
+			gameMap.at(2,18).setGround(pipe4);
+
+			WarpPipe pipe5 = new WarpPipe(lavaMap.at(0,0),"to Lava Zone!!!");
+			gameMap.at(2,2).setGround(pipe5);
+
+			WarpPipe pipe6 = new WarpPipe(lavaMap.at(0,0),"to Lava Zone!!!");
+			gameMap.at(10,12).setGround(pipe6);
 
 			WarpPipe pipeReturn = new WarpPipe(gameMap.locationOf(mario), "to Normal World");
 //			pipe2.allowableActions(mario, gameMap.locationOf(mario)," back to normal world");
 //			pipeReturn.addTeleportActions(mario, gameMap.at(mario.getOriLocation().x(), mario.getOriLocation().y())," back to normal world");
-
 //			pipeReturn.addTeleportActionsReturn(mario, gameMap.at(0,0)," back to normal world");
 			lavaMap.at(0,0).setGround(pipeReturn);
 
-//			world.addPlayer(mario, lavaMap.at(15, 10));
 
 			Actor toad = new Toad();
 			gameMap.addActor(toad, gameMap.at(42, 11));
